828f977ad0e337cc74c3c092797d76faf5ec3b35
Lars Martens <larsmartens2@gmail.com>
2022-04-26 22:31:38 +0200

    added postgres db and created student table

--- src/main/java/com/example/springtest/student/Student.java
+++ src/main/java/com/example/springtest/student/Student.java
@@ -2,6 +2,13 @@ package com.example.springtest.student;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +16,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+   @Id
+   @SequenceGenerator(
+         name = "student_sequence",
+         sequenceName = "student_sequence",
+         allocationSize = 1)
+   @GeneratedValue(
+         strategy = GenerationType.SEQUENCE,
+         generator = "student_sequence"
+         )
    private Long id;
    private String name;
    private String email;
