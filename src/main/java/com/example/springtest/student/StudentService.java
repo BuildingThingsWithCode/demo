4304f595c28502f208754703a59dcb7ae0be8a1e
Lars Martens <larsmartens2@gmail.com>
2022-04-27 19:32:41 +0200

    added a studentRepository, adapted the studentService to use this repository

--- src/main/java/com/example/springtest/student/StudentService.java
+++ src/main/java/com/example/springtest/student/StudentService.java
@@ -1,21 +1,21 @@
 package com.example.springtest.student;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 
-   public List<Student> getStudents() {
-      List<Student> studentList = new ArrayList<Student>();
-      studentList.add(new Student(1L, "Mariam", "mariam@gmail.com", LocalDate.of(2000, Month.JANUARY, 15), 22));
-      studentList.add(new Student(2L, "Dirk", "dirk@gmail.com", LocalDate.of(1998, Month.JULY, 16), 24));
-      return studentList;
+   private final StudentRepository studentRepository;
 
+   @Autowired
+   public StudentService(StudentRepository studentRepository) {
+      this.studentRepository = studentRepository;
    }
 
+   public List<Student> getStudents() {
+      return studentRepository.findAll();
+   }
 }
